2c61272c7247b4b90ca54653965641060b88cd8f
Alex Kobylansky <alex.kobylansky@gmail.com>
2022-09-14 10:45:00 +0300

    Application structure made

---
+++ src/components/employees-list-item/EmployeesListItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const EmployeesListItem = () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
---
+++ src/components/info/Info.jsx
@@ -0,0 +1,7 @@
+
+export const Info = () => {
+  return (
+    <div className="info">
+    </div>
+  );
+}
