a96b7f32eb1bca5b6ae4f69f147f4c0cb3a3a798
Alex Kobylansky <alex.kobylansky@gmail.com>
2022-09-15 20:37:37 +0300

    Added property onClick to Button.jsx

--- src/ui/button/Button.jsx
+++ src/ui/button/Button.jsx
@@ -1,9 +1,10 @@
 import './button.scss';
 
-export const Button = ({type, classes, children}) => {
+export const Button = ({type, classes, children, onClick}) => {
   return (
     <button className={classes}
             type={type}
+            onClick={onClick}
     >
       {children}
     </button>
